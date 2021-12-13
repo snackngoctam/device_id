7573fe7ea8880c5801479f7bef7b33a91799b2b9
leovu <leovu0703vn@gmail.com>
2021-12-13 14:59:03 +0700

    add web uid

--- lib/device_id.dart
+++ lib/device_id.dart
@@ -1,20 +1,30 @@
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
-
+import 'package:crypto/crypto.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:device_info/device_info.dart';
 import 'package:flutter/services.dart';
+import 'package:platform_device_id/platform_device_id.dart';
 
 class DeviceId {
   static Future<String> get imei async {
-    if (Platform.isAndroid){
-      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      return androidInfo.androidId;
+    if(kIsWeb) {
+      String deviceId = await PlatformDeviceId.getDeviceId;
+      var digest = sha1.convert(utf8.encode(deviceId));
+      return '$digest';
     }
     else {
-      MethodChannel _channel = MethodChannel("flutter.io/iOSIMEI");
-      return await _channel.invokeMethod<String>('iOSIMEI');
+      if (Platform.isAndroid){
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        return androidInfo.androidId;
+      }
+      else {
+        MethodChannel _channel = MethodChannel("flutter.io/iOSIMEI");
+        return await _channel.invokeMethod<String>('iOSIMEI');
+      }
     }
   }
 }
